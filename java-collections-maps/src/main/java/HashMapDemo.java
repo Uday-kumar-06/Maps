e4fd04a1e26759d82201b8f3336fd4a7f2f0c30d
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-01-12 23:44:51 +0530

    added some code

--- java-collections-maps/src/main/java/HashMapDemo.java
+++ java-collections-maps/src/main/java/HashMapDemo.java
@@ -1,2 +1,19 @@
-public class HashMapDemo {
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo{
+    public static void main(String [] args){
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1, "Akshit");
+        map.put(2,"uday");
+        map.put(3,"Kella");
+
+
+        for(Map.Entry<Integer,String> entry:map.entrySet()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+            entry.setValue("20");
+            System.out.println(entry.hashCode());
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+    }
 }
